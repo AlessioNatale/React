f950034f362c8837723e3b04afb8ac157976f43d
AlessioNatale <alessionatale12@gmail.com>
2023-10-05 11:56:30 +0200

    commit-es-03_04

--- esercizi/src/App.jsx
+++ esercizi/src/App.jsx
@@ -1,11 +1,20 @@
 
 
 function App() {
-
+  const name = "Jonh"
   return (
     <>
+    <h1>Hello, {name}</h1>
     </>
   )
 }
 
 export default App
+
+// Esercizio 03
+// Cosa succede se la variabile name non viene passata alla funzione?
+// il risultato dovrebbe essere undefined, quindi vedremo solo Hello
+
+// Esercizio 04
+// Cosa succede se la name variabile contiene un'espressione JSX invece di una stringa?
+//verra renderizzata in codice html e javascript
