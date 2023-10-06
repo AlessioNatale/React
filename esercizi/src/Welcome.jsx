51e19963fd9f892a62f080de878cc3a99aaf93a7
AlessioNatale <alessionatale12@gmail.com>
2023-10-06 15:15:41 +0200

    commit-es-24

--- esercizi/src/Welcome.jsx
+++ esercizi/src/Welcome.jsx
@@ -2,7 +2,7 @@ export function Welcome({name, age}){
     return(
         <dir>
             <p>Welcome, {name}!</p>
-            {age > 18 && age < 65 ? <p>Your age is {age}</p> : null}
+            {name === "Jonh" && age > 18 && age < 65 ? <p>Your age is {age}</p> : null}
         </dir>
     ) 
 }
