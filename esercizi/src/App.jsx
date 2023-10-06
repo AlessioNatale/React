3b3b86d3f1ff81cf2af17b5aa469670a7fa63a99
AlessioNatale <alessionatale12@gmail.com>
2023-10-06 10:22:54 +0200

    commit-es-14

--- esercizi/src/App.jsx
+++ esercizi/src/App.jsx
@@ -1,15 +1,14 @@
 import { Hello } from "./Hello";
+import { Message } from "./Message";
 
 export function App(){
     return(
         <div>
             <Hello/>
-            <Hello/>
-            <Hello/>
-            <Hello/>
+            <Message/>
         </div>
     )
 }
 
-// È possibile utilizzare il Hello componente più di una volta nel Appcomponente? Cosa succede se lo fai?
-//Si è possibile farlo e come si puù notare riproduce in pagina più volte il contenuto del componente Hello
+// Puoi eseguire il rendering del componente Messaggio direttamente all'interno del componente App? Cosa succede se lo fai?
+//Si è possibile e come si può vedere anche in pagina si vede sia il contenuto del componente hello e sia quello di Message
