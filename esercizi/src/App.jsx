97b2c0247ccacdef1b6d9ffab26453e1579e0755
AlessioNatale <alessionatale12@gmail.com>
2023-10-06 10:46:20 +0200

    commit-es-16

--- esercizi/src/App.jsx
+++ esercizi/src/App.jsx
@@ -1,14 +1,11 @@
-import { Hello } from "./Hello";
-import { Message } from "./Message";
+import { Welcome } from "./Welcome";
+
 
 export function App(){
     return(
         <div>
-            <Hello/>
-            <Message/>
+           <Welcome name="Jonh"/>
         </div>
     )
 }
 
-// Puoi eseguire il rendering del componente Messaggio direttamente all'interno del componente App? Cosa succede se lo fai?
-//Si è possibile e come si può vedere anche in pagina si vede sia il contenuto del componente hello e sia quello di Message
