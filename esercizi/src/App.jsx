671419220f171850b497c0890b5a5a8295ca4807
AlessioNatale <alessionatale12@gmail.com>
2023-10-06 10:17:46 +0200

    commit-es-13

--- esercizi/src/App.jsx
+++ esercizi/src/App.jsx
@@ -4,6 +4,12 @@ export function App(){
     return(
         <div>
             <Hello/>
+            <Hello/>
+            <Hello/>
+            <Hello/>
         </div>
     )
 }
+
+// È possibile utilizzare il Hello componente più di una volta nel Appcomponente? Cosa succede se lo fai?
+//Si è possibile farlo e come si puù notare riproduce in pagina più volte il contenuto del componente Hello
