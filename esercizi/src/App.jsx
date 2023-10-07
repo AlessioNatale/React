ccf3f6a6b3834d0499e8ac3faa5be4624442db20
AlessioNatale <alessionatale12@gmail.com>
2023-10-07 11:08:13 +0200

    commit-es-17

--- esercizi/src/App.jsx
+++ esercizi/src/App.jsx
@@ -9,3 +9,10 @@ export function App(){
     )
 }
 
+// Cosa succede se nameal Welcomecomponente non viene passata alcuna prop?
+// Puoi impostare un valore predefinito per l' nameoggetto di scena?
+
+//Se non passo alcuna props vedrei soltanto Hello, quindi visto che devo passare un valore predefinito
+//nel componente Welcome ne ho messo uno fisso, per fare questo forse era più utile fare con l'operatore ternario
+//però ho visto che non ci siamo ancora arrivati quindi non l'ho usato
+
