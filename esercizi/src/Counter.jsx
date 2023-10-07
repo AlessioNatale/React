a0b59d741a96333bdc4d8415e79af87a31444be7
AlessioNatale <alessionatale12@gmail.com>
2023-10-07 17:48:13 +0200

    commit-es-33

--- esercizi/src/Counter.jsx
+++ esercizi/src/Counter.jsx
@@ -7,11 +7,20 @@ export function Counter ({value = 0}){
     function handleCounterIncrement (){
         setCounter((c) => c + 1)
     }
+     function handelCounterDecrement (){
+        setCounter((c) => c - 1)
+     }
+
+     function handleCounterReset(){
+        setCounter(0)
+     }
 
     return(
         <div>
             <CounterDisplay intialValue={counter} />
             <button onClick={handleCounterIncrement}>Increment</button>
+            <button onClick={handelCounterDecrement}>Decrement</button>
+            <button onClick={handleCounterReset}>Reset</button>
         </div>
     )
 }
