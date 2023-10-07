15e9795cecbd4430d18754acd6dbbf5747434cf9
AlessioNatale <alessionatale12@gmail.com>
2023-10-07 12:13:26 +0200

    commit-es-17

--- esercizi/src/App.jsx
+++ esercizi/src/App.jsx
@@ -4,7 +4,7 @@ import { Welcome } from "./Welcome";
 export function App(){
     return(
         <div>
-           <Welcome/>
+           <Welcome name="pippo"/>
         </div>
     )
 }
@@ -13,6 +13,5 @@ export function App(){
 // Puoi impostare un valore predefinito per l' nameoggetto di scena?
 
 //Se non passo alcuna props vedrei soltanto Hello, quindi visto che devo passare un valore predefinito
-//nel componente Welcome ne ho messo uno fisso, per fare questo forse era più utile fare con l'operatore ternario
-//però ho visto che non ci siamo ancora arrivati quindi non l'ho usato
+//nel componente Welcome ne ho messo uno predefinito
 
--- esercizi/src/Welcome.jsx
+++ esercizi/src/Welcome.jsx
@@ -1,4 +1,3 @@
-export function Welcome(){
-    const name = "Jonh"
+export function Welcome({name= "Jonh"}){
     return <p>Welcome, {name}!</p>
 }
