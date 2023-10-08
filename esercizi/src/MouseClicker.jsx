a0c8ca0925c7733e7ebffe3594b5800bdfb4c1d6
AlessioNatale <alessionatale12@gmail.com>
2023-10-08 21:05:48 +0200

    commit-es-37

--- esercizi/src/MouseClicker.jsx
+++ esercizi/src/MouseClicker.jsx
@@ -1,7 +1,10 @@
 export function MouseClicker(){
     function handleButtonClick(event){
-        console.log(event.target.name);
+        console.log(event.currentTarget.name);
     }
 
-    return <button name="one" onClick={handleButtonClick}>Click me!</button>
+    return <button name="one" onClick={handleButtonClick}>
+        <img width={24} height={24} />
+        Click me!
+        </button>
 }
