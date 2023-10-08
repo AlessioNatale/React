629d2ee6164f123d0e74aaadf25c75c2f9426f2d
AlessioNatale <alessionatale12@gmail.com>
2023-10-08 21:17:55 +0200

    commit-es-38

--- esercizi/src/MouseClicker.jsx
+++ esercizi/src/MouseClicker.jsx
@@ -4,7 +4,11 @@ export function MouseClicker(){
     }
 
     return <button name="one" onClick={handleButtonClick}>
-        <img width={24} height={24} />
+        <img onClick={handleButtonClick} width={24} height={24} />
         Click me!
         </button>
 }
+
+// Il name attributo del pulsante viene comunque stampato sulla console quando si fa clic sull'immagine? Perché?
+//Si, perche sto usando l'evento currentTarget, se non volessi visualizzarlo, quindi visualizzare name solo quando 
+// sono effettivamente sulla parte interresata dovrei usare solo target
