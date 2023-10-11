5c89bc877376a5c4524ebe2abd6a8f297e5f2280
AlessioNatale <alessionatale12@gmail.com>
2023-10-11 18:01:41 +0200

    commit-es-44

--- esercizi/src/Login.jsx
+++ esercizi/src/Login.jsx
@@ -28,14 +28,17 @@ export function Login() {
         alert("Login")
     }
 
+    function resetForm(){
+        return setData(createData())
+    }
+
     return (
         <div>
             <input name="username" value={data.username} onChange={handleInputChange} />
             <input name="password" value={data.password} onChange={handleInputChange} />
             <input name="session" type="checkbox" checked={data.session} onChange={handleInputChange} />
             <button disabled={!data.username || ! data.password} onClick={onLogin}>Login</button>
+            <button onClick={resetForm}>Reset</button>
         </div>
     );
 }
-
-//la parte finale della consegna non mi è chiara, la funzione OnLogin cosa deve passare, vabbe spero che il concetto dell'esercizio sia buono 
