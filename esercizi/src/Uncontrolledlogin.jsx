3d0d259be296e72085267a2160531559028e073d
AlessioNatale <alessionatale12@gmail.com>
2023-10-13 21:01:09 +0200

    commit-es-47

--- esercizi/src/Uncontrolledlogin.jsx
+++ esercizi/src/Uncontrolledlogin.jsx
@@ -1,6 +1,21 @@
 export function UncontrolledForm(){
+    function handleFormSubmit(event){
+        event.preventDefault()
+
+        const username = event.target.elements.namedItem("username").value
+        const password = event.target.elements.namedItem("password").value
+
+        const data = {
+            username,
+            password
+        }
+
+        console.log(data);
+    }
+
+
     return(
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <h1>My Uncontroller Form</h1>
           <input name="username" />
           <input name="password" type="password"/>
