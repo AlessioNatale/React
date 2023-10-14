d65613386e3a93ffea818247c6edcdbac02be887
AlessioNatale <alessionatale12@gmail.com>
2023-10-14 15:06:25 +0200

    commit-es-51

--- esercizi/src/FocusableInput.jsx
+++ esercizi/src/FocusableInput.jsx
@@ -1,9 +1,21 @@
 import { useEffect, useRef } from "react"
 
 export function FocusableInput(){
+    const mountedRef = useRef(false)
+
     const inputRef = useRef(null)
 
+    
+
     useEffect(() =>{
+        if(!mountedRef.current){
+            mountedRef.current = true
+            console.log("montato per la prima volta")
+        }// else{
+        //      console.log("secondo montaggio");
+        // }
+        //cosi vedremo anche quando vieni montato nuovamente
+
         inputRef.current?.focus()
     }, [])
 
