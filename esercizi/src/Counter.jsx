c28a92ae4b28c14951082cb375ef7ffb0c28b10d
AlessioNatale <alessionatale12@gmail.com>
2023-10-15 11:56:24 +0200

    commit-es-52

--- esercizi/src/Counter.jsx
+++ esercizi/src/Counter.jsx
@@ -1,30 +1,43 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { CounterDisplay } from "./CounterDiplay";
 
-export function Counter ({value = 0}){
-    const [counter, setCounter] = useState(value)
+export function Counter({ value = 0 }) {
+  const [counter, setCounter] = useState(value);
+  const directionRef = useRef(null); 
 
-    useEffect(() => {
-        console.log(`Il valore corrente del contatore è ${counter}`);
-    },[counter])
-
-    function handleCounterIncrement (){
-        setCounter((c) => c + 1)
+  useEffect(() => {
+    console.log(`Il valore corrente del contatore è ${counter}`);
+    
+    if (counter >= value) {
+      directionRef.current = "down";
+    } else if (counter <= value) {
+      directionRef.current = "up";
     }
-     function handelCounterDecrement (){
-        setCounter((c) => c - 1)
-     }
+  }, [counter, value]);
 
-     function handleCounterReset(){
-        setCounter(0)
-     }
+  function handleCounterIncrement() {
+    setCounter((c) => c + 1);
+  }
+  function handelCounterDecrement() {
+    setCounter((c) => c - 1);
+  }
 
-    return(
-        <div>
-            <CounterDisplay intialValue={counter} />
-            <button onClick={handleCounterIncrement}>Increment</button>
-            <button onClick={handelCounterDecrement}>Decrement</button>
-            <button onClick={handleCounterReset}>Reset</button>
-        </div>
-    )
+  function handleCounterReset() {
+    setCounter(0);
+  }
+
+  useEffect(() => {
+    if (directionRef.current !== null) {
+      console.log(`Direction: ${directionRef.current}`);
+    }
+  }, [directionRef.current]);
+
+  return (
+    <div>
+      <CounterDisplay intialValue={counter} />
+      <button onClick={handleCounterIncrement}>Increment</button>
+      <button onClick={handelCounterDecrement}>Decrement</button>
+      <button onClick={handleCounterReset}>Reset</button>
+    </div>
+  );
 }
