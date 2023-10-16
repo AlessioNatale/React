2a2b84bd5fe1f216285756dc9466784d3ab7e6fb
AlessioNatale <alessionatale12@gmail.com>
2023-10-16 15:14:30 +0200

    commit-es-69

--- esercizi/src/Container.jsx
+++ esercizi/src/Container.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
+
 export function Container({title, children}){
+    const [collapsed, setCollapsed] = useState(false) 
+
+    function handleCollapsed(){
+        setCollapsed((t) => !t)
+    }
+
+
     return (
         <div className="app">
-            <div className="app-title">{title}</div>
-            <div className="app-children">{children}</div>
+            <div className="app-title">
+                {title} <button onClick={handleCollapsed}>Toggle</button>
+            </div>
+            {!collapsed && <div className="app-children">{children}</div>}
         </div>
     )
 }
