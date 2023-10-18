d37bb3bc7f38eef03aa5055c94d25845f50960f7
AlessioNatale <alessionatale12@gmail.com>
2023-10-18 10:21:06 +0200

    commit-es-75

--- esercizi/src/Counter.jsx
+++ esercizi/src/Counter.jsx
@@ -1,8 +1,33 @@
 import { useEffect, useState } from "react";
 import { CounterDisplay } from "./CounterDiplay";
 
+
+function useCounter (initialValue = 0) {
+    const [counter, setCounter] = useState(initialValue)
+
+    function handleCounterIncrement (){
+        setCounter((c) => c + 1)
+    }
+     function handelCounterDecrement (){
+        setCounter((c) => c - 1)
+     }
+
+     function handleCounterReset(){
+        setCounter(0)
+     }
+
+     return{
+        counter: counter,
+        onIncrement: handleCounterIncrement,
+        onDecrement: handelCounterDecrement,
+        onReset: handleCounterReset
+     }
+}
+
 export function Counter ({value = 0}){
     const [counter, setCounter] = useState(value)
+
+    
 
     useEffect(() => {
         console.log(`Il valore corrente del contatore è ${counter}`);
