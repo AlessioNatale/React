ea171f516d1b268bd2df6e85700b77b2ae5a884a
AlessioNatale <alessionatale12@gmail.com>
2023-10-18 11:56:34 +0200

    commit-es-30

--- esercizi/src/Counter.jsx
+++ esercizi/src/Counter.jsx
@@ -7,6 +7,11 @@ export function Counter (){
         setCounter((c) => c + 1)
     }
 
+    //Quando si chiama la funzione "setter" per incrementare il contatore, il parametro deve essere una funzione o un valore immediato? Perché?
+
+    //deve essere una funzione e non un vaolre immediato perchè ci poddono essere aggiornamenti asincroni e in più perchè cosi riusciamo 
+    //a tenere traccia dell'aggiornamneto precedente, così da garantire che gli aggiornamenti siano fatti sullo stato precedente
+
     return(
         <div>
             <h2>{counter}</h2>
