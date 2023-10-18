93288e2f052b858d04acaa5aac96dabe5e52fd1e
AlessioNatale <alessionatale12@gmail.com>
2023-10-18 14:27:31 +0200

    commit-es-75

--- esercizi/src/Counter.jsx
+++ esercizi/src/Counter.jsx
@@ -1,29 +1,8 @@
-import { useState } from "react";
 import { CounterDisplay } from "./CounterDiplay";
+import { useCounter } from "./CounterLogic";
 
 
-function useCounter(initialValue = 0) {
-  const [counter, setCounter] = useState(initialValue);
 
-  function handleCounterIncrement() {
-    setCounter((c) => c + 1);
-  }
-
-  function handleCounterDecrement() {
-    setCounter((c) => c - 1);
-  }
-
-  function handleCounterReset() {
-    setCounter(0);
-  }
-
-  return {
-    counter: counter,
-    onIncrement: handleCounterIncrement,
-    onDecrement: handleCounterDecrement,
-    onReset: handleCounterReset,
-  };
-}
 
 export function Counter({initialValue = 0}) {
   const { counter, onIncrement, onDecrement, onReset } = useCounter(initialValue);
