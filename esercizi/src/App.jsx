fdec80c831e378e4cef755ea45bf11d4e18f35c9
AlessioNatale <alessionatale12@gmail.com>
2023-10-23 13:54:47 +0200

    comit-es-84

--- esercizi/src/App.jsx
+++ esercizi/src/App.jsx
@@ -15,6 +15,7 @@ import { Welcome } from "./Welcome";
 import { GithubUser } from "./GithubUser";
 import { GithubUsers } from "./GithubUsers";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
   const [language, setLanguage] = useState("en");
@@ -32,6 +33,7 @@ export function App() {
         <Routes>
           <Route path="/" element={<Welcome name={"jonh"} />} />
           <Route path="/counter" element={<Counter/>}/>
+          <Route path="/users" element={<ShowGithubUser/>}/>
         </Routes>
       </Container>
     </BrowserRouter>
