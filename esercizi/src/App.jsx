ffdf723cc9d5a747bc81b74145a546ca4df49b02
AlessioNatale <alessionatale12@gmail.com>
2023-10-24 14:19:23 +0200

    commit-es-88

--- esercizi/src/App.jsx
+++ esercizi/src/App.jsx
@@ -41,13 +41,13 @@ export function App() {
           <Route path="/" element={<Welcome name={"jonh"} />} />
           <Route path="/counter" element={<Counter/>}/>
           <Route path="/users" element={<GithubUsers/>}>
-            <Route path= "/users/: username" element={<ShowGithubUser/>}/>
+            <Route path= "/users/:username" element={<ShowGithubUser/>}/>
             <Route index element={<p>Add a user and select it</p>}/>
           </Route>
            
         <Route path ="*"element={<NotFound/>} />
         </Routes>
-        <Link to={"/"}>Welcome</Link> | <Link to={"/counter"}>Counter</Link> | <Link to={"/users"}>UserGithub</Link>
+        <Link to={"/"}>Welcome</Link> | <Link to={"counter"}>Counter</Link> | <Link to={"users"}>UserGithub</Link>
       </Container>
     </BrowserRouter>
   );
