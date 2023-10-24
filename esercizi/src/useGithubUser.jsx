c8237107cf60e327a8bbe69d283942114c5b0b2c
AlessioNatale <alessionatale12@gmail.com>
2023-10-24 18:12:16 +0200

    commit-es-90

--- esercizi/src/useGithubUser.jsx
+++ esercizi/src/useGithubUser.jsx
@@ -3,6 +3,15 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((response) => response.json())
 
 export function useGithubUser(username){
+  if (username === null) {
+    return {
+      user: null,
+      error: null,
+      isLoading: false,
+    };
+  }
+
+
   const {data, error} = useSWR(`https://api.github.com/users/${username}`, fetcher)  
 
     return{
