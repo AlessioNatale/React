3ce710d9eeda509885ddf938873145fd07bc4896
AlessioNatale <alessionatale12@gmail.com>
2023-11-04 15:20:42 +0100

    correzione.es-39

--- esercizi/src/MouseClicker.jsx
+++ esercizi/src/MouseClicker.jsx
@@ -3,11 +3,16 @@ export function MouseClicker(){
         console.log(event.target.name);
     }
 
+    function handleImageClick(event){
+        console.log(event.target.src);
+    }
+
     return <button name="one" onClick={handleButtonClick}>
-        <img name="pippo" onClick={handleButtonClick} width={24} height={24} />
+        <img src="https://img.freepik.com/premium-photo/dark-blue-gradient-abstract-texture-background_41691-1179.jpg" onClick={handleImageClick} width={24} height={24} />
         Click me!
         </button>
 }
+
 
 // Come è possibile impedire che l' nameattributo del pulsante venga stampato sulla console quando si fa clic sull'immagine?
 //usando il metodo target
