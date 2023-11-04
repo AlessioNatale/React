8d641447dbcc39669ec545c0079270cd0eb48ada
AlessioNatale <alessionatale12@gmail.com>
2023-11-04 14:35:21 +0100

    correzione-es-23

--- esercizi/src/Welcome.jsx
+++ esercizi/src/Welcome.jsx
@@ -1,10 +1,10 @@
 import { Age } from "./Age";
 
-export function Welcome({name}){
+export function Welcome({name, age}){
     return(
         <dir>
             <p>Welcome, {name}!</p>
-            <Age age={19}/>
+            {age > 18 && age < 65 &&<Age age={age}/>}
         </dir>
     ) 
 }
